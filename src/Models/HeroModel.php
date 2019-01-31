4e3e2779348a7c1587955a97de150155d5357c1b
B3none <ablackham2000@gmail.com>
2019-01-31 00:05:12 +0000

    Added HeroModel and started implementing composer.

--- src/Models/HeroModel.php
+++ src/Models/HeroModel.php
@@ -1,8 +1,49 @@
 <?php
 
-namespace ValveSoftware\Models;
+namespace ValveSoftware\ArtifactDeckCode\Models;
 
 class HeroModel
 {
+    protected $id;
+    protected $turn;
 
+    /**
+     * Get ID
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ID
+     *
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Get Turn
+     *
+     * @return mixed
+     */
+    public function getTurn()
+    {
+        return $this->turn;
+    }
+
+    /**
+     * Set Turn
+     *
+     * @param mixed $turn
+     */
+    public function setTurn($turn)
+    {
+        $this->turn = $turn;
+    }
 }
