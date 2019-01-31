f11395a85b6a6ba482b15266797c9a1b1b32e4ea
B3none <ablackham2000@gmail.com>
2019-01-31 00:15:39 +0000

    Added CardModel

---
+++ src/Models/CardModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ValveSoftware\ArtifactDeckCode\Models;
+
+class CardModel
+{
+    protected $id;
+    protected $count;
+
+    /**
+     * Get ID
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ID
+     *
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Get Count
+     *
+     * @return mixed
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * Set Count
+     *
+     * @param mixed $count
+     */
+    public function setCount($count)
+    {
+        $this->count = $count;
+    }
+}
